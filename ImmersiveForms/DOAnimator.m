58e1273d4fc87863f45b57c5345c1cd1fe484923
David Olesch <david@soulemobile.com>
2014-03-25 20:55:03 -0500

    Animator does more fluid fade out now.

--- ImmersiveForms/DOAnimator.m
+++ ImmersiveForms/DOAnimator.m
@@ -16,19 +16,33 @@
 
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
 {
-    UIViewController *presentingViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-    UIViewController *overlayViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController *presentingViewController;
+    UIViewController *overlayViewController;
+    double initialAlpha;
+    double finalAlpha;
+    
+    if (self.presenting) {
+        initialAlpha = 0.f;
+        finalAlpha = 0.9f;
+        presentingViewController  = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+        overlayViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    }
+    else {
+        initialAlpha = 0.9f;
+        finalAlpha = 0.f;
+        overlayViewController  = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+        presentingViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    }
     
     UIView *containerView = [transitionContext containerView];
     [containerView addSubview:presentingViewController.view];
     [containerView addSubview:overlayViewController.view];
     
-    overlayViewController.view.alpha = 0.f;
-    
+    overlayViewController.view.alpha = initialAlpha;
     NSTimeInterval transitionDuration = [self transitionDuration:transitionContext];
     [UIView animateWithDuration:transitionDuration
                      animations:^{
-                         overlayViewController.view.alpha = 0.9f;
+                         overlayViewController.view.alpha = finalAlpha;
                      } completion:^(BOOL finished) {
                          BOOL transitionWasCancelled = [transitionContext transitionWasCancelled];
                          [transitionContext completeTransition:transitionWasCancelled == NO];
